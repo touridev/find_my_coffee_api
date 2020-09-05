06eeed46d5079ff1b50763f340add3024d6e0743
touridev <ftorjob@gmail.com>
2020-09-05 20:24:54 -0300

    Setting postgis

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,7 +1,2 @@
 class Store < ApplicationRecord
-    scope :within, -> (latitude, longitude, distance_in_mile = 1) {
-        where(%{
-        ST_Distance(clinic_lonlat, 'POINT(%f %f)') < %d
-        } % [longitude, latitude, distance_in_mile * 1609.34]) # approx
-    }
 end
--- db/migrate/20200905224911_create_store_ratings.rb
+++ db/migrate/20200905224911_create_store_ratings.rb
@@ -1,8 +1,7 @@
 class CreateStoreRatings < ActiveRecord::Migration[5.2]
   def change
     create_table :store_ratings do |t|
-      t.belongs_to :rating
-      t.belongs_to :store
+
       t.timestamps
     end
   end
