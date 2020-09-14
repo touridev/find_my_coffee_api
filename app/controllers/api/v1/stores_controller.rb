c111e95ac9013bdfc4758239dfd729bcd706c610
touridev <ftorjob@gmail.com>
2020-09-14 00:02:15 -0300

    Setting form

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::StoresController < ApplicationController
     before_action :set_store, only: [:show]
 
+    def show
+    end
+
     def index
         @stores = Store.within(params[:latitude].to_f, params[:longitude].to_f)
+    end
+
+    private
+
+    def set_store
+        @store = Store.find(params[:id])
     end
 end
