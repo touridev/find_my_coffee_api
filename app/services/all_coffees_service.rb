4db50a6f717676c4a4d5eb9f75e4a454b265adc2
touridev <ftorjob@gmail.com>
2020-11-17 11:18:44 -0300

    Update all_coffees_service.rb

--- app/services/all_coffees_service.rb
+++ app/services/all_coffees_service.rb
@@ -9,7 +9,7 @@ class AllCoffeesService
   
     def call
         begin
-            base_url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=coffee+shop&location=-#{@latitude},#{@longitude}&radius=5000&key=AIzaSyAriO9z5tX1tht7YomsgWyC9BNpWMT599w"
+            base_url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=coffee+shop&location=-#{@latitude},#{@longitude}&radius=5000&key=YOUR_SECRET_KEY"
             response = RestClient.get base_url
             value = JSON.parse(response.body)
       
